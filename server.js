6fbf0b6f15fe09f09e556b5dbc12c8495fc54f07
Tony Vargas <tonyvargas@Tonys-MacBook-Pro.local>
2023-11-07 18:12:58 -0700

    import UUID

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const db = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 
