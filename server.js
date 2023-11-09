7d29af5d42fb9251e9ff4df583f0566430a92b9c
Tomas Vargas <tavargas9@gmail.com>
2023-11-08 21:05:20 -0700

    add api and html routes

--- server.js
+++ server.js
@@ -15,4 +15,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Middleware to serve assets in the public folder
-app.use(static('public'));
+app.use(express.static('public'));
+
+//API routes
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data))
+    })
+});
+
+app.post('/api/notes', (req, res) => {
+    const note = req.body
+    note.id = uuidv4()
+    db.push(note)
+    fs.writeFileSync('./db/db.json', JSON.stringify(db))
+    res.json(db)
+});
+
+//HTML Routes
+//Notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+});
+//Homepage and all other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening at localhost:${PORT}`)
+});
